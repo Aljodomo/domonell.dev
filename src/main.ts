e03ffe2a04d86733bfd12e1902df7a727f685e0d
Aljoscha Domonell <aljo.domo@gmail.com>
2022-09-28 19:26:54 +0200

    feat: router-link prefetching

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import App from './App.vue'
 import './index.scss'
 
 import {router} from "./router/router";
+import RouterPrefetch from 'vue-router-prefetch'
 
 createApp(App)
     .use(router)
+    .use(RouterPrefetch)
     .mount('#app');
