d2c4afcd265e2729d5883ba05ba9a45002f060d6
Aljoscha Domonell <aljo.domo@gmail.com>
2022-09-28 19:26:07 +0200

    fix: imports

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,5 +1,5 @@
 import HeaderLayout from "../layouts/HeaderLayout.vue"
-import SingleCardLayout from "../layouts/SingleCardLayout.vue"
+// import SingleCardLayout from "../layouts/SingleCardLayout.vue"
 
 const Home = () => import("../views/Home.vue");
 const About = () => import("../views/About.vue");
